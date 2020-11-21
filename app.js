15411754d8aee531af08529101bf36708c0418ac
PetrusSmit30025087 <smithpetrus89@gmail.com>
2020-11-21 15:31:39 +0200

    second commit

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-const express = require(express);
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-
+//middlewares 
+app.use('/posts', () => {
+    console.log('This is running');
+});
 //Routes
 app.get('/',(req,res) => {
     res.send('test');
-    
+    console.log('hello');
 });
+
+
+//connect to my Db
+mongoose.connect('mongodb+srv://Petrus:<Peru1234>@petrusdatabase.0tgzc.mongodb.net/PetrusDatabase?retryWrites=true&w=majority',{ useNewUrlParser: true }, () => console.log('connected'));
 
 app.listen(3000);
